52e767000ba6afb0ad513b58b29ee8a2e23dac6a
CeriseAndres <cerise.andres@yahoo.fr>
2019-02-15 14:21:57 +0100

    affiche comics

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-let url = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8c30a3d837c547b27a1507e1faf9f4ea&hash=25752090038406aa81c23ef594da2a2a"
-//const LIMIT = 16;
+//let url = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8c30a3d837c547b27a1507e1faf9f4ea&hash=25752090038406aa81c23ef594da2a2a"
 
-function getComicsFromApi(){
+const PUBLIC_KEY = "8c30a3d837c547b27a1507e1faf9f4ea";
+const TS = "1";
+const HASH = "25752090038406aa81c23ef594da2a2a";
 
-  let results = '';
+const LIMIT = 16;
 
+function getComicsFromApi(LIMIT, offset){
+  var urlAPI = "https://gateway.marvel.com:443/v1/public/comics?apikey="
+    + PUBLIC_KEY + "&hash=" + HASH + "&ts=" + TS;
 
-  console.log(url);
-  $.getJSON(url, {
-    id: results
+  $.getJSON(urlAPI)
+    .done(function(json){
+      displayComics(json.data.results);
     })
-    .done(function(data) {
-
-      console.log(data);
-    })
-    .fail(function(err){
-
+    .fail(function(err) {
       console.log(err);
     });
-
-};
-getComicsFromApi();
-function displayComics(comics) {
-
 };
 
-function displayPagination(total, LIMIT, offset) {
 
+function displayComics(resultsLen) {
+  $("#results").html("");
+  for(let i=0; i<resultsLen.length; i ++){
+    let imgComic = resultsLen[i].thumbnail.path +
+      '/standard_medium.' + resultsLen[i].thumbnail.extension;
+    let resultsLi = "<li class='results'><br>" + resultsLen[i].title + "</li>";
+    resultsLi += "<img class='screen' src='" + imgComic + "'>";
+    resultsLi += "<span class='title'>" + resultsLen[i].title + "</span>";
+    resultsLi += "</li>";
+    $("#results").append(resultsLi);
+    }
 };
 
-// $(document).ready(function() {
-//   $("#slider").slider();
-//   $("#search_bt").click(function(){
-//     getComicsFromApi(LIMIT, offset:0);
-//   });
-// })
+//
+// function displayPagination(total, LIMIT, offset) {
+//
+// };
+
+$(document).ready(function() {
+  $("#search_bt").click(function(){
+    getComicsFromApi(LIMIT, 0);
+  });
+})
